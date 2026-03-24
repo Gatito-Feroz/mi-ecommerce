4acc2e7eb11b9919de10e2e97cb145b00a6836a4
Gatito Feroz <ferozgatito807@gmail.com>
2026-03-24 14:02:22 -0500

    Modificación en las tarjetas de los productos:
    -ProductCard.jsx: Se agrega el cálculo del precio aplicando el descuento. Se complementa la información de cada producto.

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import "../styles/card.css";
 
-function ProductCard({product}) {
+function ProductCard({ product }) {
+  const discountedPrice = product.price - (product.price * product.discountPercentage) / 100;
+
   return (
     <div className="card">
-        <figure>
-            <img src={product.thumbnail} alt={product.title} />
-        </figure>
+      <figure>
+        <img src={product.thumbnail} alt={product.title} />
+      </figure>
+
       <h3>{product.title}</h3>
-      <p>${product.price}</p>
+
+      <p className="price">
+        <span className="old-price">${product.price} </span>
+        <span className="new-price">
+          ${discountedPrice.toFixed(2)}
+        </span>
+      </p>
+
+      <p className="discount">
+        {product.discountPercentage}% OFF
+      </p>
+
       <p>⭐ {product.rating}</p>
     </div>
   );
